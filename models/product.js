c7633384b56aa46d70810b90134ca2c63cd26ae7
Dinesh <dineshthiru.1998@gmail.com>
2019-10-04 11:36:55 +0530

    Initial commit :construction:

---
+++ models/product.js
@@ -0,0 +1,10 @@
+class Product {
+    constructor(id, ownerId, title, imageUrl, description, price) {
+        this.id = id;
+        this.ownerId = ownerId;
+        this.title= title;
+        this. description = description;
+        this. imageUrl = imageUrl;
+        this.price = price;
+    }
+}
