7331e216964fe731ade9aa3a9d3b08aa21431db0
Dinesh <dineshthiru.1998@gmail.com>
2019-10-04 20:25:38 +0530

    fontFamily set!

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React , { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
+import { AppLoading } from 'expo';
 import { Provider } from 'react-redux';
-
+import * as Font from 'expo-font';
 
 import productsReducer from './store/reducers/product';
 import ShopNavigator from './navigation/ShopNavigator';
@@ -11,8 +12,22 @@ const rootReducer = combineReducers({
 });
 
 const store = createStore(rootReducer);
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if(!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => {
+      setFontLoaded(true);
+    }} />;
+  }
+
   return (
     <Provider store = { store }>
       <ShopNavigator />
