1e24a4c5024ef670cab903dbe5ddf6d85258f650
Dinesh <dineshthiru.1998@gmail.com>
2019-10-19 00:50:15 +0530

    store orders @ :fire:base

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,7 +1,36 @@
 export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
-    return {
-        type: ADD_ORDER, 
-        orderData: { items: cartItems, amount: totalAmount }}
+    return async dispatch => {
+        const date = new Date();
+        const response = await fetch('https://shoppingapprn.firebaseio.com/orders/u1.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                cartItems,
+                totalAmount,
+                date: date.toISOString()
+            })
+        });
+        // 'aysnc','await' are alternative to 'then' in react native used to resolve promises returned
+
+        if(!response.ok) {
+            throw new Error('Something went wrong!');
+        }
+
+        const resData = await response.json();
+
+        dispatch ({
+            type: ADD_ORDER, 
+            orderData: { 
+                id: resData.name, 
+                items: cartItems, 
+                amount: totalAmount, 
+                date: date
+            }
+        });
+    };
+
 };
