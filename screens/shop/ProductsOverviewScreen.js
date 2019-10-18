f83de962dcd049595b945dcc8f255526cd698513
Dinesh <dineshthiru.1998@gmail.com>
2019-10-18 13:51:06 +0530

    fetch from firebase

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FlatList, Platform, Button } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from  'react-navigation-header-buttons';
@@ -6,11 +6,16 @@ import { HeaderButtons, Item } from  'react-navigation-header-buttons';
 import HeaderButton from '../../components/UI/HeaderButton';
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
+import * as productActions from '../../store/actions/product';
 import Colors from '../../constants/Colors';
 
 const ProductsOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts);
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(productActions.fetchProducts());
+    }, [dispatch] );
 
     const selectItemHandler = (id, title) => {
         props.navigation.navigate('ProductDetail', { 
--- store/actions/product.js
+++ store/actions/product.js
@@ -1,6 +1,35 @@
+import Product from "../../models/product";
+
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+
+export const fetchProducts = () => {
+
+    return async dispatch =>  {
+        // redux-thunk ..any async without disturbing the flow of redux action
+        const response = await fetch('https://shoppingapprn.firebaseio.com/products.json');
+        // 'aysnc','await' are alternative to 'then' in react native used to resolve promises returned
+
+        const resData = await response.json(); 
+        const loadedProducts = [];
+
+        for (const key in resData) {
+            loadedProducts.push(new Product(
+                key,
+                'u1',
+                resData[key].title,
+                resData[key].imageUrl,
+                resData[key].description,
+                resData[key].price,
+                )
+            );
+        }
+
+        dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    };
+};
 
 
 export const deleteProduct = productId => {
@@ -24,7 +53,8 @@ export const createProduct = (title, description, imageUrl, price ) => {
         });
         // 'aysnc','await' are alternative to 'then' in react native used to resolve promises returned
 
-        const resData = await response.json(); // holds the object id created under 'name' key
+        const resData = await response.json(); 
+        // holds the object id created under 'name' key
 
         dispatch({
             type: CREATE_PRODUCT, 
